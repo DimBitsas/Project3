3353d30a60ba360bc668545bf55f2098fe0237d3
Dimitris Bitsas <dimitrismplink72@gmail.com>
2018-10-27 16:11:57 +0300

    task search modifications

--- task_search.py
+++ task_search.py
@@ -13,6 +13,79 @@ REGEX_PROMPT = "Enter regex pattern"
 results_list = []
 
 
+class Search:
+    def search(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def search_scv(key, value):
+        """Search csv for value, append results list"""
+        try:
+            with open(filename, newline='') as csvfile:
+                csv_reader = csv.DictReader(csvfile, delimiter=',')
+                for row in csv_reader:
+                    if row[key] == value:
+                        results_list.append(row)
+        except FileNotFoundError:
+            print("\nError: File " + filename + " not found\nPlease create an entry first\n")
+        except IOError:
+            print("\nError opening file: " + filename)
+
+    @staticmethod
+    def search_regex_csv(key, pattern):
+        """Search csv for value, append results list"""
+        try:
+            with open(filename, newline='') as csvfile:
+                csv_reader = csv.DictReader(csvfile, delimiter=',')
+                for row in csv_reader:
+                    if re.search(r'%s' % pattern, row[key]):
+                        results_list.append(row)
+        except FileNotFoundError:
+            print("\nError: File " + filename + " not found\nPlease create an entry first\n")
+        except IOError:
+            print("\nError opening file: " + filename)
+
+
+class DateSearch(Search):
+    def __init__(self):
+        self.date = str(get_user_date())
+
+    def search(self):
+        Search.search_scv('Date', self.date)
+
+
+class TimeSearch(Search):
+    def __init__(self):
+        while True:
+            minutes = input(TIME_SPENT_PROMPT)
+            if not minutes.isdigit():
+                print("Error: Please provide a numeric value\n")
+                continue
+            self.minutes = minutes
+            break
+
+    def search(self):
+        Search.search_scv('Time', self.minutes)
+
+
+class StringSearch(Search):
+    def __init__(self):
+        self.string = input(STRING_PROMPT)
+
+    def search(self):
+        Search.search_scv('Name', self.string)
+        Search.search_scv('Notes', self.string)
+
+
+class RegexSearch(Search):
+    def __init__(self):
+        self.pattern = input(REGEX_PROMPT)
+
+    def search(self):
+        Search.search_regex_csv('Name', self.pattern)
+        Search.search_regex_csv('Notes', self.pattern)
+
+
 def show_search_results():
     """Show to the user the search results in a appropriate format"""
     if len(results_list) == 0:
@@ -30,60 +103,6 @@ def show_search_results():
             return
 
 
-def search_csv(key, value):
-    """Search csv for value, append results list"""
-    try:
-        with open(filename, newline='') as csvfile:
-            csv_reader = csv.DictReader(csvfile, delimiter=',')
-            for row in csv_reader:
-                if row[key] == value:
-                    results_list.append(row)
-    except FileNotFoundError:
-        print("\nError: File " + filename + " not found\nPlease create an entry first\n")
-    except IOError:
-        print("\nError opening file: " + filename)
-
-
-def search_regex_csv(key, pattern):
-    """Search csv for value, append results list"""
-    try:
-        with open(filename, newline='') as csvfile:
-            csv_reader = csv.DictReader(csvfile, delimiter=',')
-            for row in csv_reader:
-                if re.search(r'%s' % pattern, row[key]):
-                    results_list.append(row)
-    except FileNotFoundError:
-        print("\nError: File "+filename+" not found\nPlease create an entry first\n")
-    except IOError:
-        print("\nError opening file: " + filename)
-
-
-def date_search():
-    """Search tasks in the csv based on date input"""
-    date = get_user_date()
-    search_csv('Date', str(date))
-
-
-def time_search():
-    """Search tasks in the csv based on minutes input"""
-    minutes = input(TIME_SPENT_PROMPT)
-    search_csv('Time', minutes)
-
-
-def string_search():
-    """Search tasks in the csv based on string input"""
-    string = input(STRING_PROMPT)
-    search_csv('Name', string)
-    search_csv('Notes', string)
-
-
-def regex_search():
-    """Search tasks in the csv based on regex pattern input"""
-    pattern = input(REGEX_PROMPT)
-    search_regex_csv('Name', pattern)
-    search_regex_csv('Notes', pattern)
-
-
 def tasks_search():
     """Provide/Choose search type
        Show search results
@@ -94,14 +113,13 @@ def tasks_search():
         if user_input == 'E':
             return
         elif user_input == 'A':
-            date_search()
+            DateSearch().search()
         elif user_input == 'B':
-            time_search()
+            TimeSearch().search()
         elif user_input == 'C':
-            string_search()
+            StringSearch().search()
         elif user_input == 'D':
-            regex_search()
+            RegexSearch().search()
         else:
             print(TRY_AGAIN_PROMPT)
         show_search_results()
-
